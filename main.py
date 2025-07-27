6b51675e62dc2a04f47eae4768bbd084dd75a3b1
Jules <inezajul@gmail.com>
2025-07-27 17:15:21 +0200

    added the logic for main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+""" this file is where all the files will meet and we create the ui for implementing the project """
+from symptom_checker import SymptomChecker
+from help_menu import display_help
+
+checker = SymptomChecker()
+
+while True:
+    print("\nWelcome to the Health Symptom Checker!")
+    print("-" * 80)
+    print("[1] Check Symptoms")
+    print("[2] View Help")
+    print("[3] Exit")
+    print("-" * 80)
+
+    choice = input("> ")
+
+    if choice == "1":
+        checker.run()
+    elif choice == "2":
+        display_help()
+    elif choice == "3":
+        print("Exiting... Stay healthy!")
+        break
+    else:
+        print("Invalid option. Please choose 1, 2, or 3.")
+
