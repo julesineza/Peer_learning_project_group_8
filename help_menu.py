fdde7aff4a13e69601a6c56db85ef2eef4ab609b
julesineza <j.ineza@alustudent.com>
2025-07-28 08:10:10 +0200

    Updated help_menu.py

--- help_menu.py
+++ help_menu.py
@@ -10,14 +10,20 @@ This application allows you to:
 
 How to Use:
 - Choose option [1] to check symptoms
-- Type your symptoms separated by commas (e.g., fever, headache)
+- Type your symptoms one by one e.g : 
+            > Enter symptom: fever
+            > Enter symptom: headache
+            > Enter symptom: q(quit)
 
 IMPORTANT:
 - This tool is NOT a medical diagnosis
 - For serious symptoms, always consult a licensed healthcare provider
     """)
+    print("- Do not enter numbers or random symbols as symptoms.")
+    print("- Use common names for symptoms (e.g., 'nausea' instead of 'feeling sick').")
+    print("- This tool does NOT store your personal data.")
+    print("- If symptoms persist, seek professional medical help.\n")
     print("-" * 95)
     input("Press Enter to return to the main menu...")
 
-if __name__ == "__main__":
-    display_help()
+
