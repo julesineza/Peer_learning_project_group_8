f1a65a1c42f7c666d6e060b736e9059b8879de2b
Cashme8 <kabandagislain440@gmail.com>
2025-07-28 12:30:33 -0700

    update on main.py file

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-""" this file is where all the files will meet and we create the ui for implementing the project """
-
-# Import logic class and functions.
-
 from symptom_checker import SymptomChecker
 from help_menu import display_help
 
+def main():
+    from database import disease_db
+    checker = SymptomChecker(disease_db)
 
-checker = SymptomChecker()
+    while True:
+        print("\nWelcome to the Health Symptom Checker!")
+        print("----------------------------------------------------")
+        print("[1] Check Symptoms")
+        print("[2] View Help")
+        print("[3] Exit")
+        print("----------------------------------------------------")
+        choice = input("> ").strip()
 
-# Start the main program looping.
-while True:
-    print("\nWelcome to the Health Symptom Checker!")
-    print("-" * 80)
-    print("[1] Check Symptoms")
-    print("[2] View Help")
-    print("[3] Exit")
-    print("-" * 80)
+        if choice == "1":
+            checker.run()
 
-    choice = input("> ")
+        elif choice == "2":
+            display_help()
 
-    if choice == "1":
-        checker.run()
-    elif choice == "2":
-        display_help()
-    elif choice == "3":
-        print("Exiting... Stay healthy!")
-        break
-    else:
-        print("Invalid option. Please choose 1, 2, or 3.")
+        elif choice == "3":
+            print("Thank you for using Health Symptom Checker. Stay safe!")
+            break
 
-        
+        else:
+            print("Invalid option. Please select 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
